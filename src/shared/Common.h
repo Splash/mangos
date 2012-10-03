87316689ead5eaffacb8cd35b2c0386af2e76b1a
Dennis Csapo <denniscsapo@gmx.net>
2012-10-03 16:20:04 +0200

    Erweiterte GM Level

--- src/shared/Common.h
+++ src/shared/Common.h
@@ -181,7 +181,22 @@ enum AccountTypes
     SEC_MODERATOR      = 1,
     SEC_GAMEMASTER     = 2,
     SEC_ADMINISTRATOR  = 3,
-    SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+    SEC_DEVELOPER      = 4,
+    SEC_FULLADMIN      = 5,
+    SEC_SUPERADMIN     = 6,
+    SEC_CONSOLE        = 7                                  // must be always last in list, accounts must have less security level always also
+};
+
+enum AccountTypes
+{
+    SEC_PLAYER         = 0,
+    SEC_MODERATOR      = 1,
+    SEC_GAMEMASTER     = 2,
+    SEC_ADMINISTRATOR  = 3,
+    SEC_DEVELOPER      = 4,
+    SEC_FULLADMIN      = 5,
+    SEC_SUPERADMIN     = 6,
+    SEC_CONSOLE        = 7                                  // must be always last in list, accounts must have less security level always also
 };
 
 // Used in mangosd/realmd
